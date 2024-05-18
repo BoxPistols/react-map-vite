562da88b89316c5dd7ce2cb2b36a67dfb533e655
a.ito <10333049+BoxPistols@users.noreply.github.com>
2024-05-19 02:23:48 +0900

    header margin

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,10 @@ import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#1976d2',
+      main: '#2054bc',
     },
     secondary: {
-      main: '#dc004e',
+      main: '#3a3d55',
     },
   },
 });
@@ -20,7 +20,7 @@ const App = () => {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <div className="relative h-screen">
-        <header className="bg-gray-800 text-white px-4 py-2 flex justify-between items-center">
+        <header className="bg-gray-700 text-white px-4 py-2 flex justify-between items-center">
           <h1 className="text-2xl font-bold">Map App</h1>
           <div className="flex gap-4">
             <Button variant="contained" size="small" color="primary" className="mr-2" endIcon={<SendIcon />}>
@@ -31,7 +31,7 @@ const App = () => {
             </Button>
           </div>
         </header>
-        <main className="absolute inset-0 top-16">
+        <main className="absolute inset-0 top-12">
           <Mapbox latitude={35.6809591} longitude={139.7673068} zoom={9} />
         </main>
         <footer className="absolute bottom-0 left-0 right-0 bg-gray-200 px-4 py-2 flex justify-center">
