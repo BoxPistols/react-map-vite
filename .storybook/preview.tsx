5a2837b1531a230337ff35c69ecd690538c595eb
a.ito <10333049+BoxPistols@users.noreply.github.com>
2024-05-19 05:05:36 +0900

    Setting Design Token(Color/Typography) for Storybook

---
+++ .storybook/preview.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { ThemeProvider } from '@mui/system';
+import { CssBaseline } from '@mui/material';
+import { theme } from '../src/lib/theme';
+
+import type { Preview } from '@storybook/react';
+import '../src/index.css';
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Story />
+      </ThemeProvider>
+    ),
+  ],
+};
+
+export default preview;
