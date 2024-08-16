9628f2bacc53860581d0450889c00c3de95232a5
a.ito <10333049+BoxPistols@users.noreply.github.com>
2024-08-16 23:01:29 +0900

    token update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,21 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 import { colorData } from './src/lib/themes/colorToken'
 
+// ヘルパー関数
+const createColorSet = (colorKey) => ({
+  main: colorData[colorKey].main,
+  dark: colorData[colorKey].dark,
+  light: colorData[colorKey].light,
+  lighter: colorData[colorKey].lighter,
+})
+
+const createSimpleColorSet = (colorKey) => ({
+  ...Object.entries(colorData[colorKey]).reduce((acc, [key, value]) => {
+    acc[key] = value
+    return acc
+  }, {}),
+})
+
 module.exports = {
   darkMode: 'class',
   content: [
@@ -20,71 +35,24 @@ module.exports = {
         ],
       },
       colors: {
-        primary: {
-          main: colorData.primary.main,
-          dark: colorData.primary.dark,
-          light: colorData.primary.light,
-          lighter: colorData.primary.lighter,
-        },
-        secondary: {
-          main: colorData.secondary.main,
-          dark: colorData.secondary.dark,
-          light: colorData.secondary.light,
-          lighter: colorData.secondary.lighter,
-        },
-        error: {
-          main: colorData.error.main,
-          dark: colorData.error.dark,
-          light: colorData.error.light,
-          lighter: colorData.error.lighter,
-        },
-        warning: {
-          main: colorData.warning.main,
-          dark: colorData.warning.dark,
-          light: colorData.warning.light,
-          lighter: colorData.warning.lighter,
-        },
-        info: {
-          main: colorData.info.main,
-          dark: colorData.info.dark,
-          light: colorData.info.light,
-          lighter: colorData.info.lighter,
-        },
-        success: {
-          main: colorData.success.main,
-          dark: colorData.success.dark,
-          light: colorData.success.light,
-          lighter: colorData.success.lighter,
-        },
-        text: {
-          primary: colorData.text.primary,
-          secondary: colorData.text.secondary,
-          disabled: colorData.text.disabled,
-          hint: colorData.text.hint,
-        },
-        background: {
-          paper: colorData.background.paper,
-          default: colorData.background.default,
-        },
+        primary: createColorSet('primary'),
+        secondary: createColorSet('secondary'),
+        error: createColorSet('error'),
+        warning: createColorSet('warning'),
+        info: createColorSet('info'),
+        success: createColorSet('success'),
+        text: createSimpleColorSet('text'),
+        background: createSimpleColorSet('background'),
         divider: colorData.divider,
-        action: {
-          active: colorData.action.active,
-          hover: colorData.action.hover,
-          selected: colorData.action.selected,
-          disabled: colorData.action.disabled,
-          disabledBackground: colorData.action.disabledBackground,
-        },
-        common: {
-          black: colorData.common.black,
-          white: colorData.common.white,
-        },
+        action: createSimpleColorSet('action'),
+        common: createSimpleColorSet('common'),
       },
       typography: (theme) => ({
         DEFAULT: {
           css: {
-            color: theme('colors.priimary.main'),
+            color: theme('colors.primary.main'),
             a: {
-              color: theme('colors.priimary.main'),
+              color: theme('colors.primary.main'),
               '&:hover': {
                 color: theme('colors.primary.light'),
               },
